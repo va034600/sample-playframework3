1bf9c45ce5c7a609fea777f4ee0833bf2c5608e8
va034600 <va034600@gmail.com>
2024-04-02 08:55:08 +0900

    Output the response as "json"

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import play.api.libs.json.Json
+
 import javax.inject.Inject
 import play.api.mvc._
 
@@ -7,7 +9,8 @@ class HelloController @Inject() (cc: ControllerComponents)
   extends AbstractController(cc) {
 
   def hello(): Action[AnyContent] = {
-    val result: Result = Ok("Hello World")
+    val map = Map(1 -> "a", 2 -> "b", 3 -> "c")
+    val result: Result = Ok(Json.toJson(map))
     Action(result.as("text/plain"))
   }
 }
